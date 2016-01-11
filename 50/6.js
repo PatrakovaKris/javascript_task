c80724a71414bc257cd7179b07fe92748089d0ff
Kristina Patrakova <Kristina_Patrakova@epam.com>
2016-01-11 10:58:25 +0400

    add txt

--- 50/6.js
+++ 50/6.js
@@ -1,3 +1,16 @@
 /**
  * Created by Kristina_Patrakova on 12/30/2015.
  */
+function countChar(string, ch) {
+    var counted = 0;
+    for (var i = 0; i < string.length; i++)
+        if (string.charAt(i) == ch)
+            counted += 1;
+    return counted;
+}
+
+function countBs(string) {
+    return countChar(string, "B");
+}
+
+console.log(countBs("BBBB"));
